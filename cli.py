e2291a7b3eeacbf961178983f258fd0ca31274e0
sagar-sehgal <sagar.r16@iiits.in>
2020-01-03 20:47:42 +0530

    shifted from url parameters to API style

--- cli.py
+++ cli.py
@@ -15,14 +15,16 @@ API_URL="http://localhost:5000/"
 
 if(len(sys.argv)>=3):
 	if(command == "get"):
-		rep = requests.get("{}get/?key={}".format(API_URL,key))
-		print(rep.text)
+		# rep = requests.get("{}get/?key={}".format(API_URL,key))
+		response = requests.get(url = "{}get/".format(API_URL), params = {'key': key}) 
+		print(response.text)
 		
 	elif(command == "set"):
 		if(len(sys.argv) >= 4):	
 			value = sys.argv[3]
-			rep = requests.get("{}set/?key={}&value={}".format(API_URL,key,value))
-			print(rep.text)
+			# rep = requests.get("{}set/?key={}&value={}".format(API_URL,key,value))
+			response = requests.get(url = "{}set/".format(API_URL), params = {'key': key, 'value': value}) 
+			print(response.text)
 		else:
 			print("value not provided")
 	
