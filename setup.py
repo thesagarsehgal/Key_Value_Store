32e7dc43011406e96e3407fcd0e667044f0c4441
sagar-sehgal <sagar.r16@iiits.in>
2020-01-03 20:46:20 +0530

    updated setup.py so that it can delete the table and reform it if it already exists

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import sqlite3
 conn = sqlite3.connect('./app_data.db')
-conn.execute("create table data ( data_key VARCHAR(50) PRIMARY KEY NOT NULL, data_value INT NOT NULL);")
+conn.execute("drop table if exists data;")
+conn.execute("create table data ( data_key VARCHAR(50) PRIMARY KEY NOT NULL, data_value VARCHAR(50) NOT NULL);")
 conn.close()
