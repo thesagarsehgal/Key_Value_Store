3c86c8828faa5fd262c6d1c2f666dbbe3c7711de
sagar-sehgal <sagar.r16@iiits.in>
2020-01-03 18:58:11 +0530

    modified cli.py to be more flexible and work when all the values are provided

--- cli.py
+++ cli.py
@@ -1,11 +1,33 @@
 import sys
+import requests
 
-command = sys.argv[1]
-key = sys.argv[2]
+if(len(sys.argv)>=2):
+	command = sys.argv[1]
+else:
+	print("action not provided")
 
-if(command == "get"):
-	print("get {}".format(key))
+if(len(sys.argv)>=3):
+	key = sys.argv[2]
+else:
+	print("key not provided")
 
-elif(command == "set"):
-	value = sys.argv[3]
-	print("set {} {}".format(key,value))	
+API_URL="http://localhost:5000/"
+
+if(len(sys.argv)>=3):
+	if(command == "get"):
+		rep = requests.get("{}get/?key={}".format(API_URL,key))
+		print(rep.text)
+		
+	elif(command == "set"):
+		if(len(sys.argv) >= 4):	
+			value = sys.argv[3]
+			rep = requests.get("{}set/?key={}&value={}".format(API_URL,key,value))
+			print(rep.text)
+		else:
+			print("value not provided")
+	
+	else:
+		print("wroong command provided")
+
+else:
+	print("insufficient arguments provided")
