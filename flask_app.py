1f9261953db035f3184f52c7fab07078ded04a6a
sagar-sehgal <sagar.r16@iiits.in>
2020-01-03 12:40:24 +0530

    added code to coonect to the database and save the code in the database

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,38 @@
 from flask import Flask, request
 import sqlite3
 
+DATABASE = './app_data.db'
+
 app=Flask(__name__)
+
+def get_connection():
+	global DATABASE
+	conn=sqlite3.connect(DATABASE)
+	return conn
 
 @app.route("/set/")
 def set():
 	key=request.args.get("key")
 	value=request.args.get("value")
-	print(">>>>>>>>>>>>>>>>>>>>>>>>>>set",key,value)
+	conn=get_connection()
+	conn.execute("insert into data (data_key, data_value) VALUES ('{}',{});".format(key,value))
+	conn.commit()
+	print("----------------------------------------")
+	print(key,value)
+	print("----------------------------------------")
+	conn.close()
 	return None
 
 @app.route("/get/")
 def get():
 	key=request.args.get("key")
-	print(">>>>>>>>>>>>>>>>>>>>>>>>>>get",key)
+	conn=get_connection()
+	data=conn.execute("select data_key, data_value from data where data_key = '{}';".format(key))
+	print("----------------------------------------")
+	for k,v in data:
+		print("KEY={}\tVALUE={}".format(k,v))
+	print("----------------------------------------")
+	conn.close()
 	return None
 
 if(__name__=="__main__"):
