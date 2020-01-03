5b8fbf28bf4bda57116067a399b13f14177dfc10
sagar-sehgal <sagar.r16@iiits.in>
2020-01-03 19:00:13 +0530

    now can returns a string as response, also  if the key is not speciified

--- flask_app.py
+++ flask_app.py
@@ -14,28 +14,37 @@ def get_connection():
 def set():
 	key=request.args.get("key")
 	value=request.args.get("value")
+
 	conn=get_connection()
 	conn.execute("insert or replace into data values('{}',{});".format(key,value))
 	conn.commit()
+	
 	print("----------------------------------------")
-	print({"key":key,"value":value})
+	print("set",{"key":key,"value":value})
 	print("----------------------------------------")
+	
 	conn.close()
-	return None
+	
+	return str("{}:{} stored sucessfully".format(key,value))
 
 @app.route("/get/")
 def get():
 	key=request.args.get("key")
+	
 	conn=get_connection()
 	data=conn.execute("select data_key, data_value from data where data_key = '{}';".format(key))
+	
 	print("----------------------------------------")
-	for k,v in data:
-		key=k
-		value=v
-		print({"key":key,"value":value})
+	ans_key,ans_value = None, None
+	for k1,v1 in data:
+		ans_key=k1
+		ans_value=v1
+	print("get",{"key":ans_key,"value":ans_value})
 	print("----------------------------------------")
+	
 	conn.close()
-	return None
+	# check for None
+	return str("{}".format(ans_value))
 
 if(__name__=="__main__"):
 	app.run(debug=True)
