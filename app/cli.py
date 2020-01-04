1cc30e98bd66c1b203d963dd25e5829df67d4e00
sagar-sehgal <sagar.r16@iiits.in>
2020-01-04 15:09:35 +0530

    comments added

--- app/cli.py
+++ app/cli.py
@@ -1,30 +1,44 @@
 import sys
+
+# inorder to make requests to the database 
 import requests
 
+# second argument would be to make a SET or GET request
 if(len(sys.argv)>=2):
+	# get the command from the first index
 	command = sys.argv[1]
 else:
-	print("action not provided")
+	print("action to perform not provided")
 
+# third argument would contain the key to query
 if(len(sys.argv)>=3):
+	# get the key from the second index
 	key = sys.argv[2]
 else:
 	print("key not provided")
 
+# get the URL of the API
 API_URL="http://0.0.0.0:5000/"
 
+# if the command 3 arguments i.e  the command and key are available 
 if(len(sys.argv)>=3):
+
 	if(command == "get"):
-		# rep = requests.get("{}get/?key={}".format(API_URL,key))
+		
+		# send request to the API and with the parameters
 		response = requests.get(url = "{}get/".format(API_URL), params = {'key': key}) 
 		print(response.text)
 		
 	elif(command == "set"):
+		
+		# check if the value correspondig to the query is priovided
 		if(len(sys.argv) >= 4):	
 			value = sys.argv[3]
-			# rep = requests.get("{}set/?key={}&value={}".format(API_URL,key,value))
+
+			# send request to the API and with the parameters
 			response = requests.get(url = "{}set/".format(API_URL), params = {'key': key, 'value': value}) 
 			print(response.text)
+		
 		else:
 			print("value not provided")
 	
--- app/flask_app.py
+++ app/flask_app.py
@@ -1,54 +1,90 @@
 from flask import Flask, request
+
+# importing sqlite3 ... bcz woud be using SQLite to store the key value pairs
 import sqlite3
 
+# the database name
 DATABASE = './app_data.db'
 
+# initializing the application 
 app=Flask(__name__)
 
+# a function to get connection to the database
 def get_connection():
+	''' gets connected to the database and returns the coonection variable '''
 	global DATABASE
 	conn=sqlite3.connect(DATABASE)
 	return conn
 
+# function to set the key,value pairs .... uses GET and POST both methods
 @app.route("/set/",methods=["GET","POST"])
 def set():
-	key=request.args.get("key")
-	value=request.args.get("value")
+	'''sets the keys and value pairs'''
 
-	conn=get_connection()
+	# gets the key from the request which was recieved
+	key = request.args.get("key")
+	# gets the key from the request which was recieved
+	value = request.args.get("value")
+
+	# gets the connection to the database
+	conn = get_connection()
 	
+	# locking the databse
 	with conn:
-		conn.execute("insert or replace into data values('{}','{}');".format(key,value))
+		# execute the statment to update/insert a key value pair.
+		# If the key would exist already, then it would be updated to the new value,
+		# else a new key would be formed with the corresponding value  
+		conn.execute("insert or replace into data values('{}', '{}');".format(key, value))
+		
+		# commiting the changes the in the above statement 
 		conn.commit()
 	
 	print("----------------------------------------")
 	print("set",{"key":key,"value":value})
 	print("----------------------------------------")
 	
+	# closing the connection with the databse
 	conn.close()
 	
-	return str("{}:{} stored sucessfully".format(key,value))
+	# return the  corresponding statement
+	return str("{}:{} stored sucessfully".format(key, value))
 
+# function to get the key,value pairs .... uses only GET method .... bcz the database would only be read
 @app.route("/get/", methods=["GET"])
 def get():
-	key=request.args.get("key")
+	'''gets the value for a giccen key '''
+
+	# gets the key from the request which as recieved
+	key = request.args.get("key")
 	
-	conn=get_connection()
+	# gets the connection variable
+	conn = get_connection()
 	
+	# locking the database
 	with conn:
-		data=conn.execute("select data_key, data_value from data where data_key = '{}';".format(key))
+		# sql quesry to query over the database for the given key
+		data = conn.execute("select data_key, data_value from data where data_key = '{}';".format(key))
 	
-	print("----------------------------------------")
-	ans_key,ans_value = None, None
+	print("----------------------------------------")	
+	# sets the key and the value to be None  
+	ans_key, ans_value = None, None
+
+	# iterates over ros to get the key and value
 	for k1,v1 in data:
 		ans_key=k1
 		ans_value=v1
+	
+	# if there was no such key, then the value corresponding to it would be None
+
 	print("get",{"key":key,"value":ans_value})
 	print("----------------------------------------")
 	
+	# close the connection to the database
 	conn.close()
-	# check for None
+	
+	# return the corresponding statement
 	return str("{}".format(ans_value))
 
 if(__name__=="__main__"):
+	# run the app at 0.0.0.0 on porn no. 5000
 	app.run(host="0.0.0.0", port= "5000" ,debug=False,threaded=True)
