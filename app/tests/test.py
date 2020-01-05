f530aa842cf621ba8d82f033f7fc6592a91bfa0c
sagar-sehgal <sagar.r16@iiits.in>
2020-01-05 11:24:48 +0530

    testing added

---
+++ app/tests/test.py
@@ -0,0 +1,58 @@
+from flask_app import app
+import unittest
+import json
+
+class FlaskTestCase(unittest.TestCase):
+
+	def test_reset(self):
+		reseter = app.test_client(self)
+		response = reseter.get('/reset/')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data,"Database Reset".encode())
+
+	def test_set_key(self):
+		reseter = app.test_client(self)
+		response = reseter.get('/reset/')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data,"Database Reset".encode())
+
+		key = "k1"
+		value = "v1"
+		
+		setter = app.test_client(self)
+		response = setter.get('/set/?key={}&value={}'.format(key,value))
+		self.assertEqual(response.data,"{}:{} stored sucessfully".format(key, value).encode())
+	
+	def test_get_key_not_preset(self):
+		reseter = app.test_client(self)
+		response = reseter.get('/reset/')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data,"Database Reset".encode())
+
+		key = "k1"
+		
+		getter = app.test_client(self)
+		response = getter.get('/get/?key={}'.format(key))
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data,"None".encode())
+
+	def test_get_key_preset(self):
+		reseter = app.test_client(self)
+		response = reseter.get('/reset/')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data,"Database Reset".encode())
+
+		key = "k1"
+		value = "v1"
+		
+		setter = app.test_client(self)
+		response = setter.get('/set/?key={}&value={}'.format(key,value))
+		self.assertEqual(response.data,"{}:{} stored sucessfully".format(key, value).encode())
+		
+		getter = app.test_client(self)
+		response = getter.get('/get/?key={}'.format(key) )
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data,"{}".format(value).encode())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ cli.py
@@ -0,0 +1,59 @@
+import sys
+
+# inorder to make requests to the database 
+import requests
+
+# second argument would be to make a SET or GET request
+if(len(sys.argv)>=2):
+	# get the command from the first index
+	command = sys.argv[1]
+else:
+	print("action to perform not provided")
+
+# third argument would contain the key to query
+if(len(sys.argv)>=3):
+	# get the key from the second index
+	key = sys.argv[2]
+else:
+	print("key not provided")
+
+# get the URL of the API
+API_URL="http://0.0.0.0:5000/"
+# API_URL="https://sagar-key-value-store.herokuapp.com/"
+
+
+# if the command 3 arguments i.e  the command and key are available 
+if(len(sys.argv)>=2):
+
+	if(command == "get"):
+		
+		# send request to the API and with the parameters
+		if(len(sys.argv) >= 3):	
+			response = requests.get(url = "{}get/".format(API_URL), params = {'key': key}) 
+			print(response.text)
+		else:
+			print("key not provided")
+		
+	elif(command == "set"):
+		
+		# check if the value correspondig to the query is priovided
+		if(len(sys.argv) >= 4):	
+			value = sys.argv[3]
+
+			# send request to the API and with the parameters
+			response = requests.get(url = "{}set/".format(API_URL), params = {'key': key, 'value': value}) 
+			print(response.text)
+		
+		else:
+			print("value not provided")
+	
+	elif(command == "reset"):
+
+		response = requests.get(url = "{}reset/".format(API_URL)) 
+		print(response.text)
+
+	else:
+		print("wrong command provided")
+
+else:
+	print("insufficient arguments provided")
